b18f943a1d6634c78248b2f69550bd18246a30a9
AlonsoMartinToledano <alonsomtgm@gmail.com>
2019-10-17 18:50:22 +0200

    RickAndMortyGraphQL

--- RickAndMortyGraphQL/src/app.js
+++ RickAndMortyGraphQL/src/app.js
@@ -1,48 +1,82 @@
-import { fetchData } from './fetchdata';
-import { GraphQLServer } from 'graphql-yoga'
+ import { fetchData } from './fetchdata';
+ import { GraphQLServer } from 'graphql-yoga'
 
 // rickymorty entry point
 const url = 'https://rickandmortyapi.com/api/character/';
 
-const typeDefs = `
+const runApp = data => {
+  const typeDefs = `
 type Query{
-  hello(name: String!, edad: Int): String!
-  me: Persona!
-  nombre: [String!]!
+  character(id: Int!): Character!
+  characters(page: Int, pageSize: Int, name: String, status: String, planet: String):[Character!]!
+  planets: [String!]!
 }
 
-type Persona{
+type Character{
+  id: Int!
   name: String!
-  age: Int!
+  status: String!
+  planet: String!
 }
 `
 
 const resolvers = {
   Query: {
-    hello: (parent, args, ctx, info) => {
-      if (args.edad)
-        return `Hello ${args.name}, tienes ${args.edad} años prro`
-      else
-        return `Hello ${args.name}`
-    },
-    me: () => {
-      return{
-        name: "Alonso",
-        age: 12
+    character: (parent, args, ctx, info) => {
+      const result = data.find(obj => obj.id == args.id);
+      
+      if (result){
+        return {
+          id: result.id,
+          name: result.name,
+          status: result.status,
+          planet: result.location.name
+        }
       }
+      else return null;
     },
-    nombre: () => ["Hola", "de que vas"]
+
+    characters: (parent, args, ctx, info) => {
+      const page = args.page || 1;
+      const pageSize = args.pageSize || 20;
+      let first = pageSize*(page - 1);
+      let last = first + pageSize;
+      let arrayResults = data.slice();
+
+      if(args.name){
+        arrayResults = arrayResults.filter(obj => obj.name.includes(args.name));
+      }
+      if(args.status){
+        arrayResults = arrayResults.filter(obj => obj.status.includes(args.status));
+      }
+      if(args.planetas){
+        arrayResults = arrayResults.filter(obj => obj.location.name.includes(args.planet));
+      }
+
+      return arrayResults
+        .slice(first, last)
+        .map(character => {
+          return{
+            id: character.id,
+            name: character.name,
+            status: character.status,
+            planet: character.location.name
+          }
+      });
+    },
+
+    planets: () => {
+      const array = [];
+      data.forEach( (elem, i) => {
+        array.push(data[i].location.name);
+      })
+
+      let result = array.filter((value, actualIndex, arr) => arr.indexOf(value) === actualIndex);
+      return result;
+    }
   }
 }
 
-/**
- * Main App
- * @param data all rickyandmorty database
- */
-const runApp = data => {
-  // data.forEach(element => {
-  //   console.log(`${element.id}: ${element.name}`);
-  // });
   const server = new GraphQLServer({typeDefs, resolvers})
   server.start();
 };
